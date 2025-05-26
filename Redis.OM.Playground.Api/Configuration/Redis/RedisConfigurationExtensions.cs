21c36468b11b0559a5f6cf5f18401a88a8ce0c8c
fabiolune <fabiolune@gmail.com>
2025-05-27 00:38:12 +0200

    Add ExcludeFromCodeCoverage attribute and remove debug output from Redis configuration

--- Redis.OM.Playground.Api/Configuration/Redis/RedisConfigurationExtensions.cs
+++ Redis.OM.Playground.Api/Configuration/Redis/RedisConfigurationExtensions.cs
@@ -1,10 +1,12 @@
 ﻿using Redis.OM;
 using Redis.OM.Contracts;
 using StackExchange.Redis;
+using System.Diagnostics.CodeAnalysis;
 using TinyFp.Extensions;
 
 namespace Redis.OM.Playground.Api.Configuration.Redis;
 
+[ExcludeFromCodeCoverage(Justification = "Initialization code that manages a connection to an actual Reids instance")]
 public static class RedisConfigurationExtensions
 {
     public static IServiceCollection AddRedisConfiguration(this IServiceCollection services, IConfiguration configuration) =>
@@ -12,7 +14,7 @@ public static class RedisConfigurationExtensions
             .GetSection(nameof(RedisConfiguration))
             .Get<RedisConfiguration>()
             .ToOption(c => c is null || c.ConnectionString is null)
-            .Map(c => c!.ConnectionString!.Tee(Console.WriteLine))
+            .Map(c => c!.ConnectionString!)
             .Map(c => ConnectionMultiplexer.Connect(c))
             .Map(m => new RedisConnectionProvider(m))
             .Match(services.AddSingleton<IRedisConnectionProvider>,
