c9324111d8b501eb734303eb18cfe20796f2dddf
fabiolune <fabiolune@gmail.com>
2025-06-05 11:57:11 +0200

    Enhance PersonEndpoint with pagination support and improve search query handling

--- Redis.OM.Playground.Api/Endpoints/PersonEndpoint.cs
+++ Redis.OM.Playground.Api/Endpoints/PersonEndpoint.cs
@@ -3,6 +3,7 @@ using Redis.OM.Contracts;
 using Redis.OM.Playground.Api.Infrastructure;
 using Redis.OM.Playground.Api.Modelling;
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using TinyFp;
 using TinyFp.Extensions;
 
@@ -10,6 +11,7 @@ namespace Redis.OM.Playground.Api.Endpoints;
 
 public class PersonEndpoint(IRedisConnectionProvider provider) : IEndpoint
 {
+    private const int MaxPageSize = 100;
     private readonly IRedisConnectionProvider _provider = provider;
 
     public WebApplication Configure(WebApplication app) =>
@@ -17,7 +19,8 @@ public class PersonEndpoint(IRedisConnectionProvider provider) : IEndpoint
             .Tee(a => a.MapPost("/person", Add))
             .Tee(a => a.MapGet("/person/{id}", GetById))
             .Tee(a => a.MapGet("/person", Get))
-            .Tee(a => a.MapGet("/person/search", Search));
+            .Tee(a => a.MapGet("/person/search", Search))
+            .Tee(a => a.MapGet("/person/list", List));
 
     private Task<IResult> Add([FromBody] Person? person) =>
         person.ToOption()
@@ -49,9 +52,17 @@ public class PersonEndpoint(IRedisConnectionProvider provider) : IEndpoint
                 (null, not null) => p => p.LastName == t.lastName,
             });
 
-    private Task<IResult> Search([FromQuery] string? q) =>
-        q.ToOption(string.IsNullOrWhiteSpace)
+    private Task<IResult> Search([FromQuery(Name = "q")] string? query) =>
+        query.ToOption(string.IsNullOrWhiteSpace)
             .Map(q => _provider.RedisCollection<Person>().Raw(q!))
+            .MapAsync(c => c.ToListAsync())
+            .MapAsync(l => new DataContainer<IList<Person>>(l))
+            .MatchAsync(Results.Ok, () => Results.StatusCode(StatusCodes.Status400BadRequest));
+
+    private Task<IResult> List([FromQuery(Name = "p")] int page = 1, [FromQuery(Name = "s")] int pageSize = 10) =>
+        (page, pageSize)
+            .ToOption(t => t.page <= 0 || t.pageSize <= 0 || pageSize > MaxPageSize)
+            .Map(t => _provider.RedisCollection<Person>(t.pageSize).Skip((page - 1) * pageSize).Take(pageSize))
             .MapAsync(c => c.ToListAsync())
             .MapAsync(l => new DataContainer<IList<Person>>(l))
             .MatchAsync(Results.Ok, () => Results.StatusCode(StatusCodes.Status400BadRequest));
